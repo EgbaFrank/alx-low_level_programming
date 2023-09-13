fe7a821774f32c7c600f76717bb1e04eda1a2891
Egba Frank <Egbafrank72@gmail.com>
2023-09-13 23:13:41 +0000

    Bugfix for all tasks

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * main - test the output of a positive or negative check
- * function
- *
- * Return: 0 success
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
