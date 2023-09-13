67ea4ecbefc9a90c1e2020b91bfcc7a426c3db17
Egba Frank <Egbafrank72@gmail.com>
2023-09-13 09:13:30 +0000

    Bugfix task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,24 +8,39 @@
 
 int main(void)
 {
-	long unsigned int fib1 = 1, fib2 = 2;
+	unsigned long int fib1 = 1, fib2 = 2;
+	unsigned long int fib1_num1, fib1_num2, fib2_num1, fib2_num2;
 	int i;
 
-	for (i = 0; i < 47; ++i)
+	for (i = 0; i < 45; ++i)
 	{
-		if (i < 46)
-		{
-			printf("%lu, %lu, ", fib1, fib2);
-		}
-
-		else
-		{
-			printf("%lu, %lu\n", fib1, fib2);
-		}
-
+		printf("%lu, %lu, ", fib1, fib2);
 		fib1 += fib2;
 		fib2 += fib1;
 	}
+	fib1_num1 = fib1 / 1000000000;
+	fib1_num2 = fib1 % 1000000000;
+	fib2_num1 = fib2 / 1000000000;
+	fib2_num2 = fib2 % 1000000000;
 
+	for (i = 0; i < 4; ++i)
+	{
+		if (i < 3)
+			printf("%lu%lu, %lu%lu, ", fib1_num1, fib1_num2, fib2_num1, fib2_num2);
+		else
+			printf("%lu%lu, %lu%lu\n", fib1_num1, fib1_num2, fib2_num1, fib2_num2);
+
+		fib1_num2 += fib2_num2;
+		if (((fib1_num2 + fib2_num2) % 1000000000) >= 1)
+			fib1_num1 += 1;
+
+		fib1_num1 += fib2_num1;
+
+		fib2_num2 += fib1_num2;
+		if (((fib2_num2 + fib1_num2) % 1000000000) >= 1)
+			fib2_num1 += 1;
+
+		fib2_num1 += fib1_num1;
+	}
 	return (0);
 }
