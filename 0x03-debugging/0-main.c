f81aeb7716a685a89ef4e1bd75178d76247a3de7
Egba Frank <Egbafrank72@gmail.com>
2023-09-13 22:33:19 +0000

    Added debugging task files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test the output of a positive or negative check
+ * function
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
