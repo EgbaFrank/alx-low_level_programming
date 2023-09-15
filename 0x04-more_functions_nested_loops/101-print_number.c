bebbec085bcef84ddeaf03b02ec31eb68573dc35
Egba Frank <Egbafrank72@gmail.com>
2023-09-15 10:59:16 +0000

    Added bugfix for task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,15 +7,17 @@
 
 void print_number(int n)
 {
+	unsigned int unsign_n = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		unsign_n *= -1;
 	}
 
-	if (n / 10)
+	if (unsign_n / 10)
 	{
-		print_number(n / 10);
+		print_number(unsign_n / 10);
 	}
-	_putchar('0' + (n % 10));
+	_putchar('0' + (unsign_n % 10));
 }
