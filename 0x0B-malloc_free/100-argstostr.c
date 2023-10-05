2309bdd8d55be198c5e6449cdc9ee79354b40e26
Egba Frank <Egbafrank72@gmail.com>
2023-10-05 20:04:59 +0000

    Added some advanced tasks

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+
+/**
+ * argstostr -  concatenates all the arguments of the program
+ * @ac: argument count
+ * @av: pointer to array of strings
+ *
+ * Return: pointer to new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, len = 0, n = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; ++i)
+	{
+		j = 0;
+
+		while (av[i][j] != '\0')
+		{
+			++j;
+		}
+
+		len += j;
+	}
+
+	str = malloc(sizeof(char) * len);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; ++i)
+	{
+		j = 0;
+
+		while (av[i][j] != '\0')
+		{
+			str[n] = av[i][j];
+			++n;
+			++j;
+		}
+		str[n] = '\n';
+		n++;
+	}
+	str[n] = '\0';
+
+	return (str);
+}
