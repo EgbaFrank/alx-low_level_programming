9184fe9d6bc08b99528c0b1775bdfce7a95c9f4d
Egba Frank <Egbafrank72@gmail.com>
2023-10-10 17:57:50 +0000

    Added project tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of file it's compiled from
+ *
+ * Return: _0 if successful
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
