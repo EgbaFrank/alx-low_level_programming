2d272e8d08ef54ad0f7e90fc089852b34948d0ec
Egba Frank <Egbafrank72@gmail.com>
2023-10-24 19:57:59 +0000

    Added minor bugfix, task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes a node at a specific 
+ * delete_nodeint_at_index - deletes a node at a specific
  * index in a listint_t list
  * @head: double pointer to the first node
  * @index: what node to be deleted
@@ -30,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	else
 	{
-		while (count != index && temp->next != NULL)
+		while (count != (index - 1) && temp->next != NULL)
 		{
 			temp = temp->next;
 			++count;
