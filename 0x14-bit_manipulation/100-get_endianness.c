16f4f8c1488ef7b0e5344d765b77d7e49f90b57d
Egba Frank <Egbafrank72@gmail.com>
2023-10-27 01:32:45 +0000

    Added an advanced task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, and 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int num = 1;
+	char *c = (char *)&num;
+
+	return (*c);
+}
