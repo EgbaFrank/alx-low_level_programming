d81ff355cb832813901adbd8a506115ac622eb55
Egba Frank <Egbafrank72@gmail.com>
2024-07-16 21:57:07 +0000

    Added some advanced tasks

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,7 +4,12 @@
 #include <stdio.h>
 #include <stddef.h>
 
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+int jump_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif /*SEARCH_ALGOS_H*/
